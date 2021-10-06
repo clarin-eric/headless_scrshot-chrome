5f187bd8ef2d88f112a360bb099f1d779466fadd
Michal Gawor <michal.j.gawor@gmail.com>
2021-10-06 13:09:31 +0200

    working headless screenshoting

--- screenshot_url.py
+++ screenshot_url.py
@@ -1,28 +1,32 @@
 import argparse
 from datetime import datetime
-from pathlib import PurePath
+import os
+from pathlib import Path
 from selenium import webdriver
-from time import sleep
+from time import sleep, strftime
 from urllib.parse import urlparse, ParseResult
 
 parser = argparse.ArgumentParser(description='Take screenshot of given URL')
 parser.add_argument('-u', '--url', type=urlparse, help='URL to take screenshot of', required=True)
-parser.add_argument('-t', '--target', type=PurePath, help='directory where to save a screenshot', required=True)
+parser.add_argument('-t', '--target', type=Path, help='directory where to save a screenshot', default='./output')
 
 args = parser.parse_args()
 
-TARGET: PurePath = args.target
+TARGET: Path = args.target
 URL: ParseResult= args.url 
 
-def make_screenshot(url: ParseResult, target: PurePath):
-    if os.path.basename(target) == '':
-        target = target / (datetime.fromtimestamp(datetime.now()) + '.png')
-
-    driver = webdriver.Chrome()
+def make_screenshot(url: ParseResult, target: Path):
+    target = target.resolve()
+        
+    target = target / (strftime("%Y%m%d%H%M%S") + '.png')
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1920x1080")
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
     driver.get(URL.geturl())
     sleep(1)
-    
-    driver.get_screenshot_as_file(target)
+
+    driver.get_screenshot_as_file(str(target))
     driver.quit()
 
 if __name__ == "__main__":
